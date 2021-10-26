d364943dc17bb98911e6960f4aa57031127356de
Queen McSteve <niko.desilva@esmt.org>
2021-10-26 21:52:43 +0200

    activity is added demo card when BORINGAPI demo button is pushed

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,1 +1,35 @@
 console.log("hello world");
+
+//testing out bored API
+// const activityDemo = $("#acitvity-demo");
+
+// async function displayActivity() {
+//   const retrievedActivity = `http://www.boredapi.com/api/activity`;
+//   const activityDetails = await fetch(`${retrievedActivity}`);
+//   //   return {
+//   //     activity: activity,
+//   //     type: type,
+//   //     participants: pax,
+//   //     price: price,
+//   //     link: link,
+//   //     key: key,
+//   //     accessibility: accessibility,
+//   //   };
+//   console.log(activityDetails);
+// }
+
+// demoButtonBoring.addEventListener("click", displayActivity);
+let boredUrl = "https://www.boredapi.com/api/activity";
+function displayActivity() {
+  fetch(boredUrl).then((response) =>
+    response
+      .json()
+      .then(
+        (activityObject) =>
+          (document.getElementById("activity-demo").innerHTML +=
+            activityObject.activity + "." + "<br>")
+      )
+  );
+}
+
+demoButtonBoring.addEventListener("click", displayActivity);
