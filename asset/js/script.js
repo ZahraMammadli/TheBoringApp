a389be5aa5f6850392691b1e64e9429ebad80440
Queen McSteve <niko.desilva@esmt.org>
2021-10-27 21:29:19 +0200

    add if else condition to embed in iframe if link exists; replace activity rather than add

--- asset/js/script.js
+++ asset/js/script.js
@@ -22,13 +22,25 @@ console.log("hello world");
 let boredUrl = "https://www.boredapi.com/api/activity";
 function displayActivity() {
   fetch(boredUrl).then((response) =>
-    response
-      .json()
-      .then(
-        (activityObject) =>
-          (document.getElementById("activity-demo").innerHTML +=
-            activityObject.activity + "." + "<br>")
-      )
+    response.json().then((activityObject) => {
+      if (activityObject.link != "") {
+        document.getElementById("activity-demo").innerHTML =
+          activityObject.activity +
+          "." +
+          "<br>" +
+          "<a href=" +
+          activityObject.link +
+          " target='_blank'>" +
+          activityObject.link +
+          "</a><br>" +
+          "<iframe src='" +
+          activityObject.link +
+          "' width='100%' height='200'></iframe><br>";
+      } else {
+        document.getElementById("activity-demo").innerHTML =
+          activityObject.activity + "." + "<br>";
+      }
+    })
   );
 }
 
