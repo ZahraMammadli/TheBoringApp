fe1bc4583787e18dbd1fcc0637904b1acb60911f
Queen McSteve <niko.desilva@esmt.org>
2021-10-30 14:32:41 +0200

    add nav bar, search youtube with activity string

--- asset/js/boring.js
+++ asset/js/boring.js
@@ -1,3 +1,5 @@
+document.getElementById("search-youtube").style.display = "none";
+document.getElementById("youtube-video").style.display = "none";
 console.log("hello world");
 
 //testing out bored API
@@ -18,13 +20,15 @@ console.log("hello world");
 //   console.log(activityDetails);
 // }
 
+//hide the youtube-search div on screen load
+
 // demoButtonBoring.addEventListener("click", displayActivity);
 let boredUrl = "https://www.boredapi.com/api/activity";
 function displayActivity() {
   fetch(boredUrl).then((response) =>
     response.json().then((activityObject) => {
       if (activityObject.link != "") {
-        document.getElementById("activity-demo").innerHTML =
+        document.getElementById("activity-card").innerHTML =
           activityObject.activity +
           "." +
           "<br>" +
@@ -36,12 +40,60 @@ function displayActivity() {
           "<iframe src='" +
           activityObject.link +
           "' width='100%' height='450'></iframe><br>";
+        console.log(activityObject.activity);
+        var searchTerm = activityObject.activity;
+        console.log(searchTerm + "!");
+        localStorage.setItem("searchterm", JSON.stringify(searchTerm));
+        document.getElementById("search-youtube").style.display = "block";
       } else {
-        document.getElementById("activity-demo").innerHTML =
+        document.getElementById("activity-card").innerHTML =
           activityObject.activity + "." + "<br>";
+        console.log(activityObject.activity);
+        var searchTerm = activityObject.activity;
+        console.log(searchTerm + "!");
+        localStorage.setItem("searchterm", JSON.stringify(searchTerm));
+        document.getElementById("search-youtube").style.display = "block";
       }
     })
   );
 }
 
-demoButtonBoring.addEventListener("click", displayActivity);
+displayActivity();
+// demoButtonBoring.addEventListener("click", displayActivity);
+
+//push bored activity into youtube search query
+
+// Swapnil's key:
+// var key = "AIzaSyDSvqHkiOyrUOr7LTw4D3tczbiBLBsRJR8";
+
+// Nabil's key:
+// var key = "AIzaSyDocdKUc8vFu4Tv30BXxP8Immc17qgXjas";
+
+// Niko's key:
+var key = "AIzaSyDTTzdwAgJf2vNOaAEcDumAALfWRgcnRxQ";
+
+//url from YouTube docs modified for my random term and API key
+// function searchYoutube() {
+//   console.log(searchTerm);
+// }
+
+function searchYoutube() {
+  var searchTerm = encodeURI(localStorage.getItem("searchterm"));
+  var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${searchTerm}&type=video&videoDuration=short&key=${key}`;
+  console.log(url);
+  document.getElementById("search-youtube").style.display = "none";
+  document.getElementById("youtube-video").style.display = "grid";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // this console log allows us to see the video id of the random video displayed
+      console.log(data.items[0].id.videoId);
+
+      // add the random video source into the video iframe on the html file
+      document.querySelector(
+        "#video-iframe"
+      ).src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
+    });
+}
+
+ytSearchButton.addEventListener("click", searchYoutube);
