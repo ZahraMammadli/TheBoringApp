9ba4eedcb5d1c8ba561221e24df76bba748f3db5
nanv <nabilnassarvelasquez@gmail.com>
2021-10-30 10:22:17 +0200

    Add more search terms

--- asset/js/youtube.js
+++ asset/js/youtube.js
@@ -13,6 +13,10 @@ const searchTerms = [
   "funny%20pets",
   "funny%20kids",
   "funny",
+  "fun%20stuff",
+  "cute%20dogs",
+  "cute%20cats",
+  "fun%20pranks",
 ];
 
 //a function to get a random search term
@@ -22,19 +26,19 @@ var getSearchTerm = () =>
 //url from YouTube docs modified for my random term and API key
 var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${getSearchTerm()}&type=video&videoDuration=short&key=${key}`;
 
-function renderVideo () {
+function renderVideo() {
   //fetch function for a random youtube video
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       // this console log allows us to see the video id of the random video displayed
       console.log(data.items[0].id.videoId);
-      
+
       // add the random video source into the video iframe on the html file
       document.querySelector(
         "#video-iframe"
       ).src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
-  });
+    });
 }
 
 // event listener for youtube button
