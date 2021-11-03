2cc8a01cf7799abf4993c5902ca34769dc30381e
Queen McSteve <niko.desilva@esmt.org>
2021-11-03 20:42:51 +0100

    add pill counters to local storage, for pill clicks and page reloads

--- asset/js/youtube.js
+++ asset/js/youtube.js
@@ -25,13 +25,24 @@ var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=
 
 //fetch function for a random youtube video
 fetch(url)
-.then((response) => response.json())
-.then((data) => {
-  // this console log allows us to see the video id of the random video displayed
-  console.log(data.items[0].id.videoId);
-  
-  // add the random video source into the video iframe on the html file
-  document.querySelector(
-    "#video-iframe"
+  .then((response) => response.json())
+  .then((data) => {
+    // this console log allows us to see the video id of the random video displayed
+    console.log(data.items[0].id.videoId);
+
+    // add the random video source into the video iframe on the html file
+    document.querySelector(
+      "#video-iframe"
     ).src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
   });
+
+//update red pill counter if page is refreshed
+window.addEventListener(
+  "unload",
+  function () {
+    var redpills = parseInt(localStorage.getItem("redpills"));
+    redpills = redpills + 1;
+    localStorage.setItem("redpills", redpills);
+  },
+  false
+);
