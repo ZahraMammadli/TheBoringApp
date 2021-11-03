9d035800a7bf0edf8c0742d641f3a33d2e28cc94
Queen McSteve <niko.desilva@esmt.org>
2021-11-03 17:28:46 +0100

    update to one button click, search terms displayed on screen

--- asset/js/youtube.js
+++ asset/js/youtube.js
@@ -8,17 +8,17 @@ var youtubeButton = document.getElementById("demo-youtube");
 var searchButton = document.getElementById("demo-search");
 
 //the search terms
-const searchTerms = [
-  "funny%20dogs",
-  "funny%20cats",
-  "funny%20pets",
-  "funny%20kids",
-  "funny",
-  "fun%20stuff",
-  "cute%20dogs",
-  "cute%20cats",
-  "fun%20pranks",
-];
+// const searchTerms = [
+//   "funny%20dogs",
+//   "funny%20cats",
+//   "funny%20pets",
+//   "funny%20kids",
+//   "funny",
+//   "fun%20stuff",
+//   "cute%20dogs",
+//   "cute%20cats",
+//   "fun%20pranks",
+// ];
 
 //random youtube search terms
 // let randomWordUrl = "https://random-word-api.herokuapp.com/word?number=33";
@@ -42,32 +42,55 @@ const searchTerms = [
 // var getSearchTerm = () =>
 //   searchTerms[Math.floor(Math.random() * searchTerms.length)];
 
-// a function to pull a random word from an API to use as a search term
-let randomWordUrl = "https://random-word-api.herokuapp.com/word?number=2";
-async function getSearchTerm() {
-  console.log("tick");
-  var pullWord = await fetch(randomWordUrl);
-  console.log("tock");
-  var searchTerm = await pullWord.json();
-  // return searchTerm;
-  console.log(searchTerm);
-  document.getElementById("search-terms").innerHTML = searchTerm;
-  localStorage.setItem("randomSearchTerm", searchTerm);
-}
-searchButton.addEventListener("click", getSearchTerm);
+// // a function to pull a random word from an API to use as a search term
+// let randomWordUrl = "https://random-word-api.herokuapp.com/word?number=2";
+// async function getSearchTerm() {
+//   console.log("tick");
+//   var pullWord = await fetch(randomWordUrl);
+//   console.log("tock");
+//   var searchTerm = await pullWord.json();
+//   // return searchTerm;
+//   console.log(searchTerm);
+//   document.getElementById("search-terms").innerHTML = searchTerm;
+//   localStorage.setItem("randomSearchTerm", searchTerm);
+// }
+// searchButton.addEventListener("click", getSearchTerm);
 
 //url from YouTube docs modified for my random term and API key
 
-function renderVideo() {
+// function renderVideo() {
+//   //fetch function for a random youtube video
+//   var randomSearchTerm = localStorage.getItem("randomSearchTerm");
+//   var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${randomSearchTerm}&type=video&videoDuration=short&key=${key}`;
+//   console.log(url);
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       // this console log allows us to see the video id of the random video displayed
+//       // console.log(data.items[0].id.videoId);
+//       // add the random video source into the video iframe on the html file
+//       document.querySelector(
+//         "#video-iframe"
+//       ).src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
+//     });
+// }
+let randomWordUrl = "https://random-word-api.herokuapp.com/word?number=2";
+async function renderVideo() {
+  var pullWord = await fetch(randomWordUrl);
+  var searchTerm = await pullWord.json();
+  console.log(searchTerm);
+  document.getElementById("search-terms").innerHTML = searchTerm;
+  // localStorage.setItem("randomSearchTerm", searchTerm);
   //fetch function for a random youtube video
-  var randomSearchTerm = localStorage.getItem("randomSearchTerm");
-  var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${randomSearchTerm}&type=video&videoDuration=short&key=${key}`;
+  // var randomSearchTerm = localStorage.getItem("randomSearchTerm");
+  var url =
+    await `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${searchTerm}&type=video&videoDuration=short&key=${key}`;
   console.log(url);
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       // this console log allows us to see the video id of the random video displayed
-      // console.log(data.items[0].id.videoId);
+      console.log(data.items[0].id.videoId);
       // add the random video source into the video iframe on the html file
       document.querySelector(
         "#video-iframe"
