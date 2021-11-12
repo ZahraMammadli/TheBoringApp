6b071176e5ae44356eabc4fb2cb7aaa01372a546
Queen McSteve <niko.desilva@esmt.org>
2021-11-12 16:53:08 +0100

    changed youtube page to pull a random search term from an api

--- asset/js/youtube.js
+++ asset/js/youtube.js
@@ -1,108 +1,106 @@
 // Nabil's key: AIzaSyDocdKUc8vFu4Tv30BXxP8Immc17qgXjas
 // Swapnil's key: AIzaSyDSvqHkiOyrUOr7LTw4D3tczbiBLBsRJR8
+// Niko's key: AIzaSyDocdKUc8vFu4Tv30BXxP8Immc17qgXjas
 
-var key = "AIzaSyDSvqHkiOyrUOr7LTw4D3tczbiBLBsRJR8";
+var key = "AIzaSyDocdKUc8vFu4Tv30BXxP8Immc17qgXjas";
 
-// the search terms to retrieve random videos
-const searchTerms = [
-  "funny%20dogs",
-  "funny%20cats",
-  "funny%20pets",
-  "funny%20kids",
-  "funny",
-  "fun%20stuff",
-  "cute%20dogs",
-  "cute%20cats",
-  "fun%20pranks",
-  "pranks",
-  "jokes",
-  "fun",
-  "cute",
-  "comedy",
-  "comedy%20dogs",
-  "comedy%20cats",
-  "comedy%20kids",
-  "doggo",
-];
+// the youtube button
+var youtubeButton = document.getElementById("demo-youtube");
+var searchButton = document.getElementById("demo-search");
 
-// the hard coded videos to be selected randomly if we consume the quota and get a 403
-const hardCodedVideos = [
-  "1HygThMLzGs",
-  "ex8SM09QChw",
-  "fB1PnrGuRwk",
-  "cjtLgiUQqgY",
-  "zp27FH9ohdU",
-  "h5dQpfIpZR0",
-  "3DJo-vG6hwA",
-  "DrMB1Ow0N74",
-];
+//the search terms
+// const searchTerms = [
+//   "funny%20dogs",
+//   "funny%20cats",
+//   "funny%20pets",
+//   "funny%20kids",
+//   "funny",
+//   "fun%20stuff",
+//   "cute%20dogs",
+//   "cute%20cats",
+//   "fun%20pranks",
+// ];
 
-//a function to get a random search term
-var getSearchTerm = () =>
-  searchTerms[Math.floor(Math.random() * (searchTerms.length - 1))];
+//random youtube search terms
+// let randomWordUrl = "https://random-word-api.herokuapp.com/word?number=33";
 
-//a function to get a random video from the list of hard coded videos
-var getHardCodedRandomVideo = () =>
-  hardCodedVideos[Math.floor(Math.random() * (hardCodedVideos.length - 1))];
+// async function getRandomWords() {
+//   var pullWords = await fetch(randomWordUrl);
+//   var searchTerm = await pullWords.json();
+
+//   console.log("ticktock2");
+//   localStorage.setItem("randomSearchTerms", searchTerm);
+//   console.log("ticktock3");
+// }
+
+// getRandomWords();
+// console.log("ticktock");
+
+// var searchTerms = [localStorage.getItem("randomSearchTerms")];
+// console.log(searchTerms);
+
+// a function to get a random search term
+// var getSearchTerm = () =>
+//   searchTerms[Math.floor(Math.random() * searchTerms.length)];
+
+// // a function to pull a random word from an API to use as a search term
+// let randomWordUrl = "https://random-word-api.herokuapp.com/word?number=2";
+// async function getSearchTerm() {
+//   console.log("tick");
+//   var pullWord = await fetch(randomWordUrl);
+//   console.log("tock");
+//   var searchTerm = await pullWord.json();
+//   // return searchTerm;
+//   console.log(searchTerm);
+//   document.getElementById("search-terms").innerHTML = searchTerm;
+//   localStorage.setItem("randomSearchTerm", searchTerm);
+// }
+// searchButton.addEventListener("click", getSearchTerm);
 
 //url from YouTube docs modified for my random term and API key
-var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${getSearchTerm()}&type=video&videoDuration=short&key=${key}`;
 
-//fetch function for a random youtube video
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    // this console log allows us to see the video id of the random video displayed
-    console.log(data.items[0].id.videoId);
+// function renderVideo() {
+//   //fetch function for a random youtube video
+//   var randomSearchTerm = localStorage.getItem("randomSearchTerm");
+//   var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${randomSearchTerm}&type=video&videoDuration=short&key=${key}`;
+//   console.log(url);
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       // this console log allows us to see the video id of the random video displayed
+//       // console.log(data.items[0].id.videoId);
+//       // add the random video source into the video iframe on the html file
+//       document.querySelector(
+//         "#video-iframe"
+//       ).src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
+//     });
+// }
+let randomWordUrl = "https://random-word-api.herokuapp.com/word?number=1";
+async function renderVideo() {
+  var pullWord = await fetch(randomWordUrl);
+  var searchTermArray = await pullWord.json();
+  console.log(searchTermArray);
+  document.getElementById("search-terms").innerHTML = searchTermArray;
+  // localStorage.setItem("randomSearchTerm", searchTerm);
+  //fetch function for a random youtube video
+  // var randomSearchTerm = localStorage.getItem("randomSearchTerm");
+  var randomSearchTerm = "elephant";
+  if (searchTermArray.length > 0) {
+    randomSearchTerm = searchTermArray.join(" and ");
+  }
+  randomSearchTerm = encodeURI(randomSearchTerm);
+  var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${randomSearchTerm}&type=video&videoDuration=short&key=${key}`;
+  // console.log(url);
+  var response = await fetch(url);
+  var data = await response.json();
+  // this console log allows us to see the video id of the random video displayed
+  // console.log(data.items[0].id.videoId);
+  // add the random video source into the video iframe on the html file
+  document.querySelector(
+    "#video-iframe"
+  ).src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
+}
+renderVideo();
 
-    // add the random video source into the video iframe on the html file
-    document.querySelector(
-      "#video-iframe"
-    ).src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
-  })
-  .catch(() => {
-    // We will show hard-coded video
-    var hardCodedRandomVideo = getHardCodedRandomVideo();
-    document.querySelector(
-      "#video-iframe"
-    ).src = `https://www.youtube.com/embed/${hardCodedRandomVideo}`;
-    console.log(
-      "oopps! we have found a 403 error. We will still show you this video so you are not sad"
-    );
-    addConsolationMessage();
-
-    function addConsolationMessage() {
-      var consolationMsg = document.createElement("H4");
-      consolationMsg.innerHTML =
-        "Oops! Seems like we found a " +
-        "<span>403 </span>" +
-        "error." +
-        "<br />" +
-        "This means we " +
-        "<span>ran out </span>" +
-        "of the assigned " +
-        "<span>YouTube quota</span>" +
-        "." +
-        "<br />" +
-        "We will still show you some " +
-        "<span>preselected videos </span>" +
-        "of funny doggos so you are not sad";
-      document.getElementById("youtube-container").appendChild(consolationMsg);
-    }
-  });
-
-//update red pill counter if page is refreshed
-window.addEventListener(
-  "unload",
-  function () {
-    var redpills = parseInt(localStorage.getItem("redpills"));
-    redpills = redpills + 1;
-    localStorage.setItem("redpills", redpills);
-  },
-  false
-);
-
-// Responsivess of the navbar
-$(document).ready(function () {
-  $(".sidenav").sidenav();
-});
+// event listener for youtube button
+// youtubeButton.addEventListener("click", renderVideo);
